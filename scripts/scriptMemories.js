d1aab23ac17b7f84dc87b6e258015ee8f3ef90ea
DariaSkripchenko <asya4832@gmail.com>
2025-08-02 15:44:36 +0300

    Добавила обновления

--- scripts/scriptMemories.js
+++ scripts/scriptMemories.js
@@ -2,9 +2,11 @@ let memoriesItems = document.querySelectorAll('.memories-item')
 
 for (let i = 0; i < memoriesItems.length; i++) {
 let memoriesButton = memoriesItems[i].querySelector('.memories-button');
+let memoriesTitle = memoriesButton.querySelector('.memories-title');
 let memoriesDescription = memoriesItems[i].querySelector('.memories-description')
 
     memoriesButton.onclick = function () {
-        memoriesDescription.classList.toggle('memories-description_on')
+        memoriesDescription.classList.toggle('memories-description_on');
+        memoriesTitle.classList.toggle('memories-title_open');
     }
 }
